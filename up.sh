5c8a9080d09276a3717af49d55d0f48de6e0da22
Anil Maharshi <devops.kphb@gmail.com>
2021-03-28 21:24:35 +0530

    finally working

--- up.sh
+++ up.sh
@@ -2,19 +2,12 @@
 
 set -e
 
-# EDIT THIS:
-#------------------------------------------------------------------------------#
 NUM_WORKER_NODES=2
 WORKER_NODES_INSTANCE_TYPE=t2.micro
-STACK_NAME=test-cluster
+STACK_NAME=eks-cluster
 KEY_PAIR_NAME=anilOhio
-#------------------------------------------------------------------------------#
 
-# Output colours
-COL='\033[1;34m'
-NOC='\033[0m'
-
-echo -e  "$COL> Deploying CloudFormation stack (may take up to 15 minutes)...$NOC"
+echo  "Deploying CloudFormation stack"
 aws cloudformation deploy \
   "$@" \
   --template-file eks.yml \
@@ -25,10 +18,10 @@ aws cloudformation deploy \
       NumWorkerNodes="$NUM_WORKER_NODES" \
       WorkerNodesInstanceType="$WORKER_NODES_INSTANCE_TYPE"
 
-echo -e "\n$COL> Updating kubeconfig file...$NOC"
+echo -e "\nUpdating kubeconfig file..."
 aws eks update-kubeconfig "$@" --name "$STACK_NAME" 
 
-echo -e "\n$COL> Configuring worker nodes (to join the cluster)...$NOC"
+echo -e "\nConfiguring worker nodes (to join the cluster)..."
 # Get worker nodes role ARN from CloudFormation stack output
 arn=$(aws cloudformation describe-stacks \
   "$@" \
@@ -52,5 +45,5 @@ data:
         - system:nodes
 EOF
 
-echo -e "\n$COL> Almost done! Cluster will be ready when all nodes have a 'Ready' status."
-echo -e "> Check it with: kubectl get nodes --watch$NOC"
+echo -e "Cluster will be ready when all nodes have a 'Ready' status."
+echo -e "Check it with command: kubectl get nodes --watch"
